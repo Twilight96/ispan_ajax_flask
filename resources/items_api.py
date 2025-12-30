65778a8ffcc299d5060fbb23d5c73ae33c6f7a7a
Emily <yy41714@gmail.com>
2025-12-30 19:06:22 +0800

    first lesson

--- resources/items_api.py
+++ resources/items_api.py
@@ -2,18 +2,23 @@ from flask_restful import Resource
 from flask import request
 
 class Items(Resource):
-    def get(self):                                                # GET /items
+    # GET /items
+    def get(self):                                                
         return {'message': '讀取所有資料'}, 200
+    # POST /items
     def post(self):    
-        data = request.get_json()
-                                          # POST /items
+        data = request.get_json()   #接收client端傳過來的json資料
+                                          
         return {'message': f'新增{data["name"]},{data["email"]}'}, 201
 
 class Item(Resource):
-    def get(self, id):                                           # GET /items/<id>   
+    # GET /items/<id> 
+    def get(self, id):                                             
         return {'message': f'根據{id}讀取資料'}, 200
+    # PUT /items/<id>
     def put(self, id):     
-        data = request.get_json()                                      # PUT /items/<id>    
-        return {'message': f'根據{id}修改{data["name"]}'}, 200   
-    def delete(self, id):                                      # DELETE /items/<id>
+        data = request.get_json ()                                          
+        return {'message': f'根據{id}修改{data["name"]}'}, 200  
+    # DELETE /items/<id> 
+    def delete(self, id):                                      
         return {'message': f'根據{id}刪除資料'},200
